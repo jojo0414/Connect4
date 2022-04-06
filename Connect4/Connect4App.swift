3898654723bec2e13b86a0f3f1d9bd4831fe5d67
jojo <jojo22123770@gmail.com>
2022-04-06 10:57:10 +0800

    Initial Commit

---
+++ Connect4/Connect4App.swift
@@ -0,0 +1,17 @@
+//
+//  Connect4App.swift
+//  Connect4
+//
+//  Created by 陳昕喬 on 2022/4/6.
+//
+
+import SwiftUI
+
+@main
+struct Connect4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Connect4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Connect4
+//
+//  Created by 陳昕喬 on 2022/4/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
